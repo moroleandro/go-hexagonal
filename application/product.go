922fd72aa9c9b4c2efb9e154014cc5026007db37
Leandro Moro <leandro.moro@globalhitss.com.br>
2021-09-17 00:04:14 -0300

    chore: create interfaces and services persistent

--- application/product.go
+++ application/product.go
@@ -1,6 +1,15 @@
 package application
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type ProductInterface interface {
 	IsValid() (bool, error)
@@ -12,21 +21,67 @@ type ProductInterface interface {
 	GetPrice() float64
 }
 
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
 const (
 	DISABLED = "disabled"
 	ENABLED  = "enabled"
 )
 
 type Product struct {
-	ID     string
-	Name   string
-	Price  float64
-	Status string
+	ID     string  `valid:"uuidv4"`
+	Name   string  `valid:"required"`
+	Price  float64 `valid:"float,optional"`
+	Status string  `valid:"required"`
 }
 
-// func (p *Product) IsValid() (bool, error) {
+func NewProduct() *Product {
+	product := Product{
+		ID:     uuid.NewV4().String(),
+		Status: DISABLED,
+	}
 
-// }
+	return &product
+}
+
+func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("The status must be enabled or disabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("The price must be greater or equal zero")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
 
 func (p *Product) Enable() error {
 	if p.Price > 0 {
@@ -37,9 +92,14 @@ func (p *Product) Enable() error {
 	return errors.New("The price must be greater than zero to enable the product")
 }
 
-// func (p *Product) Disable() error {
+func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
 
-// }
+	return errors.New("The price must zero in order to have the product disabled")
+}
 
 func (p *Product) GetID() string {
 	return p.ID
--- application/product_test.go
+++ application/product_test.go
@@ -4,12 +4,13 @@ import (
 	"testing"
 
 	"github.com/moroleandro/go-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
 func TestProduct_Enabled(t *testing.T) {
 	product := application.Product{}
-	product.Name = "Coca Cola 2L"
+	product.Name = "Coke 2L"
 	product.Status = application.DISABLED
 	product.Price = 10
 
@@ -19,4 +20,41 @@ func TestProduct_Enabled(t *testing.T) {
 	product.Price = 0
 	err = product.Enable()
 	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disabled(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Pizza"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "The price must zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Beer"
+	product.Status = application.DISABLED
+	product.Price = 15
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "The status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "The price must be greater or equal zero", err.Error())
 }
